1ba7a97e661c5ce6bd5de1582f4a8589311eb16f
wangzheyuan <wangzheyuan@xiaomi.com>
2020-04-24 17:55:48 +0800

    20200424jpa

---
+++ src/main/java/org/example/domain/Customer.java
@@ -0,0 +1,17 @@
+package org.example.domain;
+
+public class Customer {
+    private Long custId;
+
+    private String custName;
+
+    private String custSource;
+
+    private String custLevel;
+
+    private String custIndustry;
+
+    private String custPhone;
+
+    private String custAddress;
+}
